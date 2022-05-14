ec9205f05d3c4856ed5c142c10bd791b4c5128fd
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2022-05-14 18:01:34 +0200

    init

---
+++ affine.py
@@ -0,0 +1,79 @@
+from typing import Tuple, List
+
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch import Tensor
+from torch.distributions.multivariate_normal import MultivariateNormal
+
+import nf_utils as nf
+
+class Affine(nf.Flow):
+    """Affine transformation y = e^a * x + b.
+
+    Args:
+        dim (int): dimension of input/output data. int
+    """
+
+    def __init__(self, dim: int = 2):
+        """ Create and init an affine transformation. """
+        super().__init__()
+        self.dim = dim
+        self.log_scale = nn.Parameter(torch.zeros(self.dim))  # a
+        self.shift = nn.Parameter(torch.zeros(self.dim))  # b
+
+    def forward(self, x: Tensor) -> Tuple[Tensor, Tensor]:
+        """Compute the forward transformation given an input x.
+
+        Args:
+            x: input sample. shape [batch_size, dim]
+
+        Returns:
+            y: sample after forward tranformation. shape [batch_size, dim]
+            log_det_jac: log determinant of the jacobian of the forward tranformation, shape [batch_size]
+        """
+        B, D = x.shape
+
+        ##########################################################
+
+        y = torch.exp(self.log_scale) * x + self.shift
+
+        jac = torch.exp(self.log_scale)
+        log_jac = torch.log(jac[0] * jac[1])
+        log_det_jac = torch.full((B,), log_jac.item())
+
+        ##########################################################
+
+        assert y.shape == (B, D)
+        assert log_det_jac.shape == (B,)
+
+        return y, log_det_jac
+
+    def inverse(self, y: Tensor) -> Tuple[Tensor, Tensor]:
+        """Compute the inverse transformation given an input y.
+
+        Args:
+            y: input sample. shape [batch_size, dim]
+
+        Returns:
+            x: sample after inverse tranformation. shape [batch_size, dim]
+            inv_log_det_jac: log determinant of the jacobian of the inverse tranformation, shape [batch_size]
+        """
+        B, D = y.shape
+
+        ##########################################################
+        x = (y - self.shift) / torch.exp(self.log_scale)
+
+        jac = 1/torch.exp(self.log_scale)
+        log_jac = torch.log(jac[0] * jac[1])
+        inv_log_det_jac = torch.full((B,), log_jac.item())
+
+        ##########################################################
+
+        assert x.shape == (B, D)
+        assert inv_log_det_jac.shape == (B,)
+
+        return x, inv_log_det_jac
---
+++ test.py
@@ -0,0 +1,39 @@
+import torch
+
+import nf_utils as nf
+from affine import Affine
+from stacked_flows import StackedFlows
+from train import train
+
+dataset_1 = nf.CircleGaussiansDataset(n_gaussians=1, n_samples=500)
+
+
+def test_affine_forwad():
+    dataset_1 = nf.CircleGaussiansDataset(n_gaussians=1, n_samples=500)
+    x = torch.from_numpy(dataset_1.X)
+
+    affine_1 = Affine().forward(x)
+    assert torch.equal(x, Affine().inverse(affine_1[0])[0])
+
+def test_stack_log_prob():
+    dataset_1 = nf.CircleGaussiansDataset(n_gaussians=1, n_samples=500)
+    x = torch.from_numpy(dataset_1.X)
+
+    transform = [Affine(), Affine()]
+    stacked = StackedFlows(transform)
+    result = stacked.log_prob(x)
+    assert result is not None
+
+def test_stack_rsample():
+    transform = [Affine(), Affine()]
+    stacked = StackedFlows(transform)
+    stacked.rsample(10)
+
+def test_train():
+    print()
+    transform = [Affine(), Affine()]
+    model = StackedFlows(transform)
+    train(model, dataset_1)
+    assert 1 is not None
+
+
