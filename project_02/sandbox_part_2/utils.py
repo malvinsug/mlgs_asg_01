91cb3e37e4aeef6fee0ee1aa834f79816dbe3a9a
Malvin <malvin@crewmeister.com>
2022-05-29 08:37:56 +0200

    add pickling function for experimenting

--- project_02/sandbox_part_2/utils.py
+++ project_02/sandbox_part_2/utils.py
@@ -13,3 +13,14 @@ def get_mnist_data(train=True):
 def get_device():
     use_cuda = torch.cuda.is_available() 
     return torch.device("cuda" if use_cuda else "cpu")
+
+def save_variable(data,data_name):
+    import pickle
+    with open(f"{data_name}.pickle", "wb") as output_file:
+        pickle.dump(data, output_file)
+
+def load_variable(data_name):
+    import pickle
+    with open(f"{data_name}.pickle", "rb") as input_file:
+        e = pickle.load(input_file)
+    return e
